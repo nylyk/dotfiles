02c75d5557ada7ea04f64f34434466ac360d5bc2
nylyk <hi@nylyk.com>
2026-08-03 14:41:37 +0200

    changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,7 +22,7 @@ set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 
 fish_add_path $HOME/.local/bin
 fish_add_path $CARGO_HOME/bin
-fish_add_path $PNPM_HOME
+fish_add_path $PNPM_HOME/bin
 
 alias ls="eza -la --group-directories-first"
 alias cd="z"
